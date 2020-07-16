608062b00a2043528833eea82964e15ed7e6bd2d
poonam-more <pdm5682@gmail.com>
2020-07-16 10:32:23 +0530

    Initial Commit

---
+++ InductionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InductionApp
+//
+//  Created by QLC on 16/07/20.
+//  Copyright © 2020 QLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InductionAppTests/InductionAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  InductionAppTests.swift
+//  InductionAppTests
+//
+//  Created by QLC on 16/07/20.
+//  Copyright © 2020 QLC. All rights reserved.
+//
+
+import XCTest
+@testable import InductionApp
+
+class InductionAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
